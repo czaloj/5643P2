a92a30950f61fcf3c76343125c3a16fcb9ade64f
czaloj <czaloj@gmail.com>
2014-03-21 12:32:04 -0400

    Added A Wavy Cloth

---
+++ CS5643P2/CS5643P2/Cloth.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CS5643P2 {
+    public class Cloth {
+        private DynamicVertexBuffer vb;
+        public readonly int stride, rows;
+        VertexPositionNormalTexture[] verts;
+        private IndexBuffer ib;
+        public Vector3 this[int vx, int vy] {
+            get {
+                return verts[vy * stride + vx].Position;
+            }
+            set {
+                verts[vy * stride + vx].Position = value;
+                verts[vy * stride + vx + stride * rows].Position = value;
+            }
+        }
+
+        public Cloth(GraphicsDevice g, int w, int h, Vector2 s) {
+            // Scale To Patches
+            int u = w, v = h;
+            w++; h++;
+            stride = w;
+            rows = h;
+
+            // Create Vertices
+            verts = new VertexPositionNormalTexture[(w * h) << 1];
+            int i = 0;
+            for(int z = 0; z < h; z++) {
+                for(int x = 0; x < w; x++) {
+                    verts[i].Position.X = x * s.X;
+                    verts[i].Position.Y = 0;
+                    verts[i].Position.Z = z * s.Y;
+                    verts[i].TextureCoordinate.X = (float)x / (float)u * 0.5f;
+                    verts[i].TextureCoordinate.Y = (float)z / (float)v;
+                    verts[i].Normal = Vector3.UnitY;
+                    i++;
+                }
+            }
+            for(int z = 0; z < h; z++) {
+                for(int x = 0; x < w; x++) {
+                    verts[i].Position.X = x * s.X;
+                    verts[i].Position.Y = 0;
+                    verts[i].Position.Z = z * s.Y;
+                    verts[i].TextureCoordinate.X = (float)x / (float)u * 0.5f;
+                    verts[i].TextureCoordinate.X += 0.5f;
+                    verts[i].TextureCoordinate.Y = (float)z / (float)v;
+                    verts[i].Normal = -Vector3.UnitY;
+                    i++;
+                }
+            }
+
+            vb = new DynamicVertexBuffer(g, VertexPositionNormalTexture.VertexDeclaration, verts.Length, BufferUsage.WriteOnly);
+            vb.SetData(verts);
+
+            int[] inds = new int[((u * v) << 2) * 3];
+            int ii1 = 0, ii2 = ((u * v) << 1) * 3;
+            int vi1 = 0, vi2 = verts.Length >> 1;
+            for(int z = 0; z < v; z++) {
+                for(int x = 0; x < u; x++) {
+                    inds[ii1++] = vi1 + 0;
+                    inds[ii1++] = vi1 + 1;
+                    inds[ii1++] = vi1 + w + 0;
+                    inds[ii1++] = vi1 + w + 0;
+                    inds[ii1++] = vi1 + 1;
+                    inds[ii1++] = vi1 + w + 1;
+                    vi1++;
+                    inds[ii2++] = vi2 + 0;
+                    inds[ii2++] = vi2 + w + 0;
+                    inds[ii2++] = vi2 + 1;
+                    inds[ii2++] = vi2 + 1;
+                    inds[ii2++] = vi2 + w + 0;
+                    inds[ii2++] = vi2 + w + 1;
+                    vi2++;
+                }
+                vi1++;
+                vi2++;
+            }
+
+            ib = new IndexBuffer(g, IndexElementSize.ThirtyTwoBits, inds.Length, BufferUsage.WriteOnly);
+            ib.SetData(inds);
+        }
+
+        public void UpdatePositions() {
+            vb.SetData(verts);
+        }
+
+        public void SetBuffers(GraphicsDevice g) {
+            g.SetVertexBuffer(vb);
+            g.Indices = ib;
+        }
+        public void Draw(GraphicsDevice g) {
+            g.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, verts.Length, 0, ib.IndexCount / 3);
+        }
+    }
+}
--- CS5643P2/CS5643P2/Program.cs
+++ CS5643P2/CS5643P2/Program.cs
@@ -3,17 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace CS5643P2 {
-    static class Program {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+    class Program : Game {
+        GraphicsDeviceManager graphics;
+
+        public Program() {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            graphics.ApplyChanges();
+        }
+
+        Cloth cloth;
+        BasicEffect fx;
+        Texture2D t;
+        float a = 0;
+
+        protected override void Initialize() {
+            base.Initialize();
+        }
+        protected override void LoadContent() {
+            cloth = new Cloth(GraphicsDevice, 100, 100, Vector2.One * 0.1f);
+
+            int h = 17, w = h << 1;
+            t = new Texture2D(GraphicsDevice, w, h);
+            Color[] c = new Color[w * h];
+            int i = 0;
+            for(int x = 0; x < h; x++) {
+                for(int y = 0; y < h; y++) {
+                    c[y * w + x] = (i % 2 == 0) ? Color.Red : Color.Orange;
+                    c[y * w + x + h] = (i % 2 == 0) ? Color.Blue : Color.Cyan;
+                    i++;
+                }
+            }
+            t.SetData(c);
+
+
+            fx = new BasicEffect(GraphicsDevice);
+            fx.TextureEnabled = true;
+            fx.VertexColorEnabled = false;
+            fx.LightingEnabled = false;
+            fx.FogEnabled = false;
+            fx.World = Matrix.Identity;
+            fx.View = Matrix.CreateLookAt(new Vector3(12, 3, 12), Vector3.Zero, Vector3.Up);
+            fx.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 0.01f, 1000f);
+            fx.DiffuseColor = Vector3.One;
+            fx.Texture = t;
+
+            base.LoadContent();
+        }
+        protected override void UnloadContent() {
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime) {
+            a += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            a = MathHelper.WrapAngle(a);
+
+            for(int z = 0; z < cloth.rows; z++) {
+                for(int x = 0; x < cloth.stride; x++) {
+                    Vector3 p = cloth[x, z];
+                    cloth[x, z] = new Vector3(
+                        p.X,
+                        (float)(Math.Sin(p.X + a) * Math.Sin(p.Z + a)),
+                        p.Z
+                        );
+                }
+            }
+            cloth.UpdatePositions();
+
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Transparent);
+            GraphicsDevice.BlendState = BlendState.Opaque;
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
+            GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
+
+            cloth.SetBuffers(GraphicsDevice);
+            fx.CurrentTechnique.Passes[0].Apply();
+            cloth.Draw(GraphicsDevice);
+
+            GraphicsDevice.SetVertexBuffers(null);
+            GraphicsDevice.Indices = null;
+
+            base.Draw(gameTime);
+        }
+
         [STAThread]
         static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            using(Program p = new Program()) {
+                p.Run();
+            }
         }
     }
 }
